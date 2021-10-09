3c7feb7dec27713d099dde1575117d80aaf44e1c
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-10-09 20:53:15 +0900

    소스 파일 정리 및 첫 스테이지 구현

---
+++ 2DGP Game Source File/타일맵 테스.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="MainLev2.0" tilewidth="32" tileheight="32" tilecount="2448" columns="51">
+ <image source="MainLev2.0.png" width="1632" height="1536"/>
+</tileset>
--- Make_game.py
+++ Make_game.py
@@ -1,4 +1,5 @@
 import pygame
+import cv2
 
 pygame.init()# 초기화한다.
 
@@ -6,17 +7,26 @@ pygame.init()# 초기화한다.
 screen_width = 1280
 screen_height = 960
 
-pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 #화면 타이틀 설정
 pygame.display.set_caption("My Game")
 
-running = True
+running = True # 게임이 진행 중
+
+#배경 이미지들 불러오기
+background1 = pygame.image.load("C:/Users/tlseh/OneDrive/문서/GitHub/Making game/2DGP Game Source File/Tile Map.png")
+
 
 # 이벤트 루프 실행
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        if event.type == pygame.QUIT: #창이 닫히면
+            running = False# 게임 종
 
-pygame.init()
+    screen.blit(background1,(0, 0))
+
+    pygame.display.update()
+
+#pygame 종료
+pygame.quit()
