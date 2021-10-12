a852603ed527f607c6b7368554ae832c675dbb20
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-10-12 21:41:22 +0900

    이미지 파일 수정
    기존 맵이랑 맞는 캐릭터가 없어서 다시 맵 제작합니다.

--- Make_game.py
+++ Make_game.py
@@ -15,8 +15,17 @@ pygame.display.set_caption("My Game")
 running = True # 게임이 진행 중
 
 #배경 이미지들 불러오기
-background1 = pygame.image.load("C:/Users/tlseh/OneDrive/문서/GitHub/Making game/2DGP Game Source File/Tile Map.png")
+background1 = pygame.image.load("C:/Users/tlseh/OneDrive/문서/GitHub/Making game/2DGP Game Source File/Map/Tile Map 1.png")
 
+#캐릭터 불러오기
+character = pygame.image.load("C:/Users/tlseh/OneDrive/문서/GitHub/Making game/2DGP Game Source File/Character/priest1_v1_1.png")
+character_size = character.get_rect().size # 이미지 크기를 구해온다.
+character_width = character_size[0] #가로
+character_height = character_size[1] #세로
+character_x_pos = screen_width / 2 # 캐릭터의 가로 위치
+character_y_pos = screen_height / 2 # 캐릭터의 세로 위치
+
+# 캐릭터 이동 거리는 48
 
 # 이벤트 루프 실행
 while running:
@@ -25,6 +34,7 @@ while running:
             running = False# 게임 종
 
     screen.blit(background1,(0, 0))
+    screen.blit(character,(character_x_pos, character_y_pos))
 
     pygame.display.update()
 
